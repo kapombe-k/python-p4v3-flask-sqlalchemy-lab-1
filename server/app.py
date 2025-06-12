5bfea04da11ad6d211e8e29c748c0433b66e5d4c
Eugene Kapombe <kapombek@gmail.com>
2025-06-12 12:10:12 +0300

    earthquakes queried

--- server/app.py
+++ server/app.py
@@ -21,7 +21,36 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    earthquake = Earthquake.query.get(id)
 
+    if not earthquake:
+        return {"message": f"Earthquake {id} not found."}, 404
+    
+    return {'id': earthquake.id,
+                'location': earthquake.location,
+                'magnitude': earthquake.magnitude,
+                'year': earthquake.year
+    }
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    if not earthquakes:  # If no earthquakes meet the criteria
+        return {"message": f"No earthquakes found with magnitude {magnitude} or greater", "count": 0,
+            "quakes": []}
+    quakes_data = [{
+        'id': quake.id,
+        'location': quake.location,
+        'magnitude': quake.magnitude,
+        'year': quake.year
+    } for quake in earthquakes]
+    return {
+        'count': len(quakes_data),
+        'quakes': quakes_data
+    }
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
